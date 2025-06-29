4e5e09ebb1a4df311db1d23ca22dfde69aa80777
simpliKome <thelawrenzo1@gmail.com>
2025-06-29 23:46:26 +0100

    Empty commit ...

---
+++ low_level_programming/c_prog/singly_linked_list/0-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+}
---
+++ low_level_programming/c_prog/singly_linked_list/README.md
@@ -0,0 +1,6 @@
+# This Directory explores linked list as a data structure and
+# how to implemenet a linked list and also operations on linked
+# list
+#
+#
+#
