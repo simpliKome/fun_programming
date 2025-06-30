31e72ce4b7a8bac8b41489c8d03f316c7a4036ab
simpliKome <thelawrenzo1@gmail.com>
2025-06-30 04:02:56 +0100

    Added Prototype for the Function to find the length of a linked list structure [list_t]

--- low_level_programming/c_prog/singly_linked_list/lists.h
+++ low_level_programming/c_prog/singly_linked_list/lists.h
@@ -23,6 +23,6 @@ typedef struct list_s
 
 /*	Function Print All elements in a linked list list_t   */
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 
 #endif	/*  LISTS_H	  */
