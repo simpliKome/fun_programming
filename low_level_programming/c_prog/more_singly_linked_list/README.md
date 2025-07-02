64bb32c51241c806e95f1f98a3a55e59c19f488d
simpliKome <thelawrenzo1@gmail.com>
2025-07-02 01:27:00 +0100

    HEADER FILE - Contains a singly linked list data structure and prototypes of operations to perform on the data structures

---
+++ low_level_programming/c_prog/more_singly_linked_list/README.md
@@ -0,0 +1,8 @@
+# A singly linked list is a linear data structure where each element
+# called a node, contains data and a pointer (or link) to the next node
+# in the sequence
+#
+# More Task on singly linked list to further enhance my skill on data
+# structures
+
+
---
+++ low_level_programming/c_prog/more_singly_linked_list/lists.h
@@ -0,0 +1,35 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+/*	Function That prints all elements in a linked lists  */
+size_t print_listint(const listint_t *h);
+
+/*	Function that returns the length of a linked list  */
+size_t listint_len(const listint_t *h);
+
+/*	Function that adds a new node at the beginning of a linked list listint  */
+listint_t *add_nodeint(listint_t **head, const int n);
+
+/*	Function that adds a new node at the end of a linked list listint  */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/*	Function that frees a listint_t list  */
+void free_listint(listint_t *head);
+
+
+
+#endif	/*  LISTS_H  */
