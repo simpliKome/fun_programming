f613fa3c36682f19ad81cd7b2857c0dfd89e6407
simpliKome <thelawrenzo1@gmail.com>
2025-07-06 02:20:21 +0100

    HEADER FILE INCLUSION - Added prototype of the insert_nodeint.c function

--- low_level_programming/c_prog/more_singly_linked_list/lists.h
+++ low_level_programming/c_prog/more_singly_linked_list/lists.h
@@ -46,6 +46,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /*	Function that sums a listint_t list  */
 int sum_listint(listint_t *head);
 
+/*	Function that inserts node at nth index  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 
 #endif	/*  LISTS_H  */
