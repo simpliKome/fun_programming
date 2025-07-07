6e2c7da04ff8f5407a2eb6aafc7d5080705877c1
simpliKome <thelawrenzo1@gmail.com>
2025-07-08 00:29:40 +0100

    FUNCTION MODIFICATION - Function is modified to handle cases where a wrong index is passed as an argument

--- low_level_programming/c_prog/more_singly_linked_list/10-delete_nodeint.c
+++ low_level_programming/c_prog/more_singly_linked_list/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	size_t len = listint_len(*head);
 	unsigned int i = 0;
 
-	if (curr == NULL || (index >= len))
+	if ((len == 0) || (index >= len))
 		return (-1);
 
 	if (index == 0)
@@ -31,8 +31,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if ((i == index) && (curr->next != NULL))
 		{
 			temp = curr->next;
-			curr = temp->next;
-			free(curr);
+			curr->next = temp->next;
+			free(temp);
 			break;
 		}
 		curr = curr->next;
